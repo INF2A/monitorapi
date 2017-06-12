ac37bae61bd41e3db6311b34c8cfc0f7a8f0fed9
ericrutgers <eric.rutgers@protonmail.com>
2017-06-12 11:17:28 +0200

    Add project

---
+++ src/main/java/Monitor.java
@@ -0,0 +1,98 @@
+package main.java;
+
+import org.json.JSONObject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Path("/")
+public class Monitor {
+    String[] ip = {
+            "192.168.1.1",
+            "192.168.1.2",
+            "192.168.1.3",
+            "192.168.1.4",
+            "192.168.1.5",
+            "192.168.1.6",
+            "192.168.1.7"
+    };
+    String nodeName;
+
+    public boolean checkUp(String ip){
+        boolean check = false;
+        try{
+            InetAddress addr = InetAddress.getByName(ip);
+            int port = 22;
+            SocketAddress sockaddr = new InetSocketAddress(addr, port);
+
+            Socket sock = new Socket();
+
+            int timeoutMs = 100;
+            sock.connect(sockaddr, timeoutMs);
+
+            if (sock.isConnected()){
+                check =true;
+            }
+        }
+
+        catch (UnknownHostException e){
+            System.out.print(e.getStackTrace());
+        }
+        catch (IOException e){
+            System.out.print(e.getStackTrace());
+        }
+        return check;
+    }
+
+    //LOCALHOST:8086/monitor
+    @Path("/monitor")
+    @GET
+    @Produces("application/json")
+    public Response monitor() throws Exception {
+        JSONObject obj = new JSONObject();
+        for(int i =0 ; i < ip.length; i++)
+        {
+            if(checkUp(ip[i]))
+            {
+                obj.put(ip[i], true);
+            }
+            else
+            {
+                obj.put(ip[i], false);
+            }
+        }
+         return Response.status(200).entity(obj.toString()).build();
+    }
+
+    @Path("/monitor/{n}")
+    @GET
+    @Produces("application/json")
+    public Response dockerPS(@PathParam("n") String s) {
+        JSONObject obj = new JSONObject();  //Instantiate new JSONObject
+        obj.put(s, dockerCommands(s).toString());
+        //obj.put(s, ); //Put value in JSONObject
+        return Response.ok(obj.toString()).build(); /* Return HTTP status code (2xx = OK,
+                                                                                3xx = Redirection,
+                                                                                4xx = client errors,
+                                                                                5xx = server errors )
+                                                                                with JSONObject to string as entity to be printed out. */
+    }
+
+    public List<String> dockerCommands(String command){
+        List<String> commands = new ArrayList<>();
+        commands.add("docker");
+        commands.add("node");
+        commands.add("ps");
+        commands.add(command);
+        List<String> output = Shell.getInstance().runCommand(commands, false);
+        //System.out.println(output);
+        return output;
+    }
+}
---
+++ src/main/java/Shell.java
@@ -0,0 +1,48 @@
+package main.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Erwin on 5/30/2017.
+ */
+public class Shell {
+    private static Shell ourInstance = new Shell();
+
+    public static Shell getInstance() {
+        return ourInstance;
+    }
+
+
+    public List<String> runCommand(List<String> commands, boolean showShellOutputBox)  {
+        // Store the shell output
+        List<String> returnValue = new ArrayList<String>();
+
+        // call the script by creating a new process and starting it
+        ProcessBuilder pb2 = new ProcessBuilder(commands);
+        pb2.redirectError();
+
+        try {
+            Process shellScript = pb2.start();
+
+            // Create a reader for java to get the output of the script
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(shellScript.getInputStream()));
+            //        BufferedWriter stdOutput = new BufferedWriter(new OutputStreamWriter(wifiScript.getOutputStream()));
+            //        BufferedReader stdError = new BufferedReader(new InputStreamReader(wifiScript.getErrorStream()));
+
+            String s = null;
+            while ((s = stdInput.readLine()) != null) {
+                returnValue.add(s);
+            }
+
+            stdInput.close();
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+        return returnValue;
+
+    }
+}
